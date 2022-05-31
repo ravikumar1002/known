5eb2a016acefca48440877a3d6797c67040839f9
Ravi kumar <1002kumarravi@gmail.com>
2022-05-31 12:13:58 +0530

    side-bar-navigation

--- src/components/pagelayout/PageLayout.jsx
+++ src/components/pagelayout/PageLayout.jsx
@@ -1,25 +1,28 @@
+import { Grid } from "@mui/material"
 import { Header } from "../header/Header"
 import { SideBar } from "../side-nav/SideBar"
 import { SuggestionBar } from "../suggestion-box/SuggestionBar"
 
 export const PageLayout = ({ children }) => {
-            console.log(children)
+    console.log(children)
     return (
-        <div>
+        <>
             <div>
                 <Header />
             </div>
-            <div> 
-                <div>
+            <Grid container
+                sx={{ width: "100vw", padding: "1rem" }}
+            >
+                <Grid item  md={2} sm={0}>
                     <SideBar />
-                </div>
-                <div>
+                </Grid>
+                <Grid item sm={6} md={8}>
                     {children}
-                </div>
-                <div>
+                </Grid>
+                <Grid item display={{ xs: "none", md: "block" }} md={2}>
                     <SuggestionBar />
-                </div>
-            </div>
-        </div>
+                </Grid>
+            </Grid>
+        </>
     )
 }
--- src/components/side-nav/SideBar.jsx
+++ src/components/side-nav/SideBar.jsx
@@ -1,7 +1,71 @@
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
+const drawerWidth = 240;
+
+const DrawerWrapper = styled(Box)(({ theme }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    display: "flex",
+    whiteSpace: "nowrap",
+    boxSizing: "border-box",
+    height: "100vh",
+    [theme.breakpoints.down("sm")]: {
+        display: "flex",
+        width: "100%",
+    },
+}));
+
+const DrawerLists = styled(List)(({ theme }) => ({
+    [theme.breakpoints.down('sm')]: {
+        display: 'flex',
+        width: '100vw',
+        position: 'fixed',
+        justifyContent: 'space-around',
+        bottom: "0"
+
+    },
+}));
+
+const ResponsiveIcon = styled(ListItemIcon)(({ theme }) => ({
+    [theme.breakpoints.down("sm")]: {
+        minWidth: 0,
+        mr: 'auto',
+        justifyContent: 'center',
+    },
+}));
+
+
+const ResponsiveHideText = styled(ListItemText)(({ theme }) => ({
+    [theme.breakpoints.down("sm")]: {
+        display: "none",
+    },
+}));
+
 export const SideBar = () => {
     return (
-        <div>
-            sidebar
-        </div>
-    )
-}
+        <DrawerWrapper>
+            <Box>
+                <DrawerLists>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ResponsiveIcon>
+                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                </ResponsiveIcon>
+                                <ResponsiveHideText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </DrawerLists>
+            </Box>
+        </DrawerWrapper>
+    );
+};
